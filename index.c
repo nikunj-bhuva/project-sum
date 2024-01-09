5437f592a3312b496104ca73642fa402c59e8fc6
nikunj-bhuva <nikunjbhuva121@gmail.com>
2024-01-09 11:59:15 +0530

    hello

---
+++ index.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int main()
+{
+
+    //first question//
+
+//     for(int i=0; i<=5; i++)
+//     {
+//         for(int j=0; j<=5; j++)
+//         {
+//             if(i==0||j==0||i==5||j==5)
+//             {
+//                  printf("  * ");   
+//             }
+//             else
+//             {
+//                 printf("    ");
+//             }
+//         }
+//          printf("\n");
+// }
+
+    //second question//
+
+    // int array[3][3]={{8,20,30},
+    //                  {13,35,9},
+    //                  {2,7,6}};
+    // int sum=0;
+    // int average;
+
+    // for(int i=0; i<=2; i++)
+    // {
+    //     for(int j=0; j<=2; j++)
+    //     {
+    //         sum+=array[i][j];
+    //     }
+    // }
+    // average=sum/9;
+    // printf("%d",average);
+
+    //third question//
+
+    int abc[3][3]={{8,20,30},
+                    {     }
+                    }
+    return 0;
+}
